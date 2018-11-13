6775b8dc641267cb493bf3ee6e445e95c37e059c
kamhoyung <kamho.yung@gmail.com>
2018-11-13 13:10:26 +0100

    Partially Done
    5 quotes added. The button now also fetches new quotes.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,56 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
+var quotes = [
+	{
+		quote: "Work Harder.",
+		source: "Casey Neistat",
+		year: "2015",
+		citation: "Youtube"
+	},
+	{
+		quote: "...be a tourist in other perspectives.",
+		source: "Questlove",
+		year: "2018",
+		citation: "Creative Quest"
+	},
+	{
+		quote: "You can't become a good designer by staying at home and look at a book. You’ve got to be out. Talk to other people, and travel because there’s nothing better than realizing that 200km from your door, things are different.",
+		source: "Erik Spiekerman",
+		year: "2010",
+		citation: "Interview with gestalten.tv"
+	},
+	{
+		quote: "Complaining is silly. Either act or forget.",
+		source: "Stefan Sagmeister",
+	},
+	{
+		quote: "Even if your ambitions are huge, start slow, start small, build gradually, build smart.",
+		source: "Gary Vaynerchuck",
+	}
+]
+var text = "";
 
-
-
-// Create the getRandomQuuote function and name it getRandomQuote
-
+// Create the getRandomQuote function and name it getRandomQuote
+function getRandomQuote(i) {
+	var i = Math.floor(Math.random()*5);
+	//console.log(i+1);
+	return quotes[i];
+}
 
 
 // Create the printQuote funtion and name it printQuote
 
+function printQuote() {
+	var gettingTheQuote = getRandomQuote();
+	text = "<p class='quote'>" + gettingTheQuote.quote + "</p>";
+	text += "<p class='source'>" + gettingTheQuote.source + "</p>";
+	/*citation and year should only be displayed if theres a match key: value
+  	text += "<span class='citation'>" getRandomQuote().citation + "</span>";
+  	text += "<span class='year'>" getRandomQuote().year "</span></p>";
+  	*/
+  	document.getElementById('quote-box').innerHTML = text;
+}
 
 
 // This event listener will respond to "Show another quote" button clicks
