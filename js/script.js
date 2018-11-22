a22ef2207b40dd64286d4a12131e1b99e22be6b3
kamhoyung <kamho.yung@gmail.com>
2018-11-22 22:46:55 +0100

    Revision after feedback
    - Implemented template literals to spare away that many concatenations.
    - the timer now resets if the button is triggered by the user
    - the timer now counts 10 sec instead of 30.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // FSJS - Random Quote Generator
-
+var myTimer;
 
 /* A function that gets a random RGB value, but between 0 and 99 instead of 255.
 This is to ensure readability, as in too light background makes the white text hard to read */
@@ -26,29 +26,33 @@ function getRandomQuote(i) {
 // A funtion named printQuote that replaces the exisiting string in the element with the unique identifier called quote-box
 function printQuote() {
 	var chosenQuote = getRandomQuote();
-	text = "<p class='tag'>" + chosenQuote.tag + "</p>";
-	text += "<p class='quote'>" + chosenQuote.quote + "</p>";
+	text = `<p class='tag'> ${chosenQuote.tag} </p>`;
+	text += `<p class='quote'> ${chosenQuote.quote} </p>`;
 		/* conditional statement to lower the font size for quotes with a string containing more than 200 characters.
 		This is to ensure readability for smaller screen width */
 	  	if ( chosenQuote.quote.length > 200){
 			text +="<style>.quote {font-size: 1.7rem;line-height: 1.4;}</style>";
 		}
-	text += "<p class='source'>" + chosenQuote.source;
+	text += `<p class='source'> ${chosenQuote.source}`;
 		// conditional statement for showing citation and year, that will be displayed only if the key exists.
 	  	if ( "citation" in chosenQuote){
-			text += "<span class='citation'>" + chosenQuote.citation + "</span>";
+			text += `<span class='citation'> ${chosenQuote.citation} </span>`;
 	  	};
 	  	if ("year" in chosenQuote) {
-	  		text += "<span class='year'>" + chosenQuote.year + "</span>";
+	  		text += `<span class='year'> ${chosenQuote.year} </span>`;
 	  	};
-  	text += "</p>";
-  	document.body.style.background = randomColor();
-  	document.getElementById('quote-box').innerHTML = text;
-  	
+  text += "</p>";
+  document.body.style.background = randomColor();
+  document.getElementById('quote-box').innerHTML = text;
+	timer();
 }
 
-//Calling the printQuote function ever 30 seconds
-setInterval(printQuote, 30000);
+// a function in which calls the printQuote function every 10 seconds and resets the timer
+function timer() {
+	clearInterval(myTimer);
+	myTimer = setInterval(printQuote, 10000);
+}
+timer();
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
